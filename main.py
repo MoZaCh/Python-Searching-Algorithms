bdb12f07c40390233ed19e0152f695a411946017
Mohammed Choudhury <Mzchoudhury@outlook.com>
2022-08-20 14:15:20 +0100

    Linear search function developed.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 
+def linear_search(arr, value):
+    found = False
+
+    for i in arr:
+        if i == value:
+            found = True
+            break
+
+    return found
+
+
 if __name__ == '__main__':
     print("This is a test!")
