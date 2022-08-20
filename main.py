2f60cb03ce3ca0d83177eaff6b9516e601cfc9b1
Mohammed Choudhury <Mzchoudhury@outlook.com>
2022-08-20 21:52:18 +0100

    Function documentation added.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 
 def linear_search(arr, value):
+    """
+    Searches through a list for a specific element (sequentially).
+    :param arr: The array to be searched.
+    :param value: The value to be searched for.
+    :return: A boolean value True or False depending on whether the element exists in the list.
+    """
     found = False
 
     for i in arr:
