04d4ebcdbee9e35efae43292d71cf1d441d4ab93
Mohammed Choudhury <Mzchoudhury@outlook.com>
2022-08-22 14:30:32 +0100

    Binary search function completed.

--- main.py
+++ main.py
@@ -19,11 +19,40 @@ def linear_search(arr, value):
     return found
 
 
+def print_list(arr):
+
+    for i in range(len(arr)-1):
+        print(arr[i])
+    return True
+
+
+def copy_range_of_list(start, end, arr):
+    newList = []
+
+    for i in range(start, end, 1):
+        newList.append(arr[i])
+    return newList
+
+
 def binary_search(arr, value):
 
-    found = False
+    if len(arr) == 1:
+        if arr[0] == value:
+            return True
+        else:
+            return False
+    else:
+        mid = len(arr)//2  # Length of list divided by 2 to get the middle value
+        temp = []
 
-    return found
+        if arr[mid] > value:
+            temp = copy_range_of_list(0, mid, arr)
+            return binary_search(temp, value)
+        elif arr[mid] < value:
+            temp = copy_range_of_list(mid, len(arr), arr)
+            return binary_search(temp, value)
+        else:
+            return True
 
 
 if __name__ == '__main__':
@@ -31,8 +60,10 @@ if __name__ == '__main__':
     listA = [1, 2, 4, 7, 8, 6]
     listB = [10]
     listC = []
-    print(linear_search(listA, 7))
-    print(linear_search(listA, 10))
-    print(linear_search(listB, 10))
-    print(linear_search(listC, 4))
+    listD = [1, 2]
+    #print(linear_search(listA, 7))
+    #print(linear_search(listA, 10))
+    #print(linear_search(listB, 10))
+    #print(linear_search(listC, 4))
+    print(binary_search(listD, 10))
 
